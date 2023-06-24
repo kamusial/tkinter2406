0ae4756f65c8aba4e73c022f83234b59253d139c
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-06-24 09:46:24 +0200

    1

--- 1_hello.py
+++ 1_hello.py
@@ -1,7 +1,13 @@
 import tkinter as tk
 
 root = tk.Tk()
+root.title('Tkinter')
+root.geometry('600x400+100+50')
 
+message1 = tk.Label(root, text='Pierwszy napis')
+message1.pack()
 
+message2 = tk.Label(root, text='Drugi napis')
+message2.pack()
 
 root.mainloop()
